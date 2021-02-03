10d75579854dca045b8dfca2fd9f2ca01cd3a3ec
vishal <jagtap.vishal30@gmail.com>
2021-02-03 10:20:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { QueryBuilderComponent } from "@syncfusion/ej2-react-querybuilder";
+import { employeeData } from "./data-source";
+import { SampleBase } from "./sample-base";
+import { Browser } from "@syncfusion/ej2-base";
+// import './default.css';
+export class App extends SampleBase {
+  constructor() {
+    super(...arguments);
+    this.columnData = [
+      // {
+      //   field: "C1",
+      //   label: "C1",
+      //   type: "number",
+      //   operators: [
+      //     { key: "Equal", value: "equal" },
+      //     { key: "Greater than", value: "greaterthan" },
+      //     { key: "Less than", value: "lessthan" },
+      //   ],
+      // },
+      { field: "C1", label: "C1", type: "string" },
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      { field: "C2", label: "C2", type: "string" },
+      { field: "C3", label: "C3", type: "string" },
+
+      { field: "C4", label: "C4", type: "string" },
+      {
+        field: "C5",
+        label: "C5",
+        type: "date",
+        format: "dd/MM/yyyy",
+      },
+      { field: "C6", label: "C6", type: "string" },
+      { field: "C7", label: "C7", type: "string" },
+      { field: "C8", label: "C8", type: "string" },
+    ];
+    this.importRules = {
+      // 'condition': 'and',
+      // 'rules': [{
+      //         'label': 'EmployeeID',
+      //         'field': 'EmployeeID',
+      //         'type': 'number',
+      //         'operator': 'equal',
+      //         'value': 1
+      //     },
+      //     {
+      //         'label': 'Title',
+      //         'field': 'Title',
+      //         'type': 'string',
+      //         'operator': 'equal',
+      //         'value': 'Sales Manager'
+      //     }]
+    };
+  }
+  createdControl() {
+    console.log(
+      "--",
+      JSON.stringify(this.qbObj.getValidRules(this.qbObj.rule), null, 4)
+    );
+    if (Browser.isDevice) {
+      this.qbObj.summaryView = true;
+    }
+  }
+
+  updateRule(args) {
+    // this.txtAreaElem.value = this.qryBldrObj.getSqlFromRules(args.rule);
+    console.log("--", JSON.stringify(args.rule, null, 4));
+  }
+
+  render() {
+    return (
+      <div className="control-pane">
+        <div className="control-section">
+          <div className="row">
+            <div className="col-lg-12 control-section">
+              <QueryBuilderComponent
+                width="70%"
+                ruleChange={this.updateRule.bind(this)}
+                // dataSource={employeeData}
+                columns={this.columnData}
+                rule={this.importRules}
+                created={this.createdControl.bind(this)}
+                ref={(scope) => {
+                  this.qbObj = scope;
+                }}
+              ></QueryBuilderComponent>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
